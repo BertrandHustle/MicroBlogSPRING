ad23b466b0cd4d51b488252fc1357d6e1a11561f
BertrandHustle <scott.greenberg926@gmail.com>
2016-05-17 20:57:28 -0400

    beginning

---
+++ src/main/java/com/theironyard/MicroBlogSpringController.java
@@ -0,0 +1,26 @@
+package com.theironyard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class MicroBlogSpringController {
+
+    @Autowired
+    MicroBlogSpringInterface microBlogSpringInterface;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String login(Model model, HttpSession session){
+
+        //String userName = session.getAttribute("userName");
+        return "redirect:/";
+
+    }
+
+
+}
