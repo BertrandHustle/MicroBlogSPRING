f8e0c9231d60e4594729c188e8ebc443bb914450
BertrandHustle <scott.greenberg926@gmail.com>
2016-05-18 10:59:42 -0400

    login working

--- src/main/java/com/theironyard/MicroBlogSpringController.java
+++ src/main/java/com/theironyard/MicroBlogSpringController.java
@@ -1,26 +1,33 @@
 package com.theironyard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class MicroBlogSpringController {
 
     @Autowired
     MicroBlogSpringInterface microBlogSpringInterface;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String login(Model model, HttpSession session){
+    public String webRoot(Model model, HttpSession session){
 
-        //String userName = session.getAttribute("userName");
+        model.addAttribute("userName", session.getAttribute("userName"));
         return "home";
 
     }
 
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String userName, HttpSession session){
+
+        session.setAttribute("userName", userName);
+        return "redirect:/";
+
+    }
 
 }
